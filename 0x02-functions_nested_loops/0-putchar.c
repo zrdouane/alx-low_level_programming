302c9b6108467d8ea15942e7c338a04b7a0e3d31
Zrdouane <ramirdouane@gmail.com>
2023-02-21 05:36:57 -0800

    create putstr and putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,36 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - program that prints 1 char and loop to complete string that user entre,
+ * followed by a new line
+ * Return: Always 1
+ */
+void	ft_putchar(char c)
+{
+	write(0, &c, 1);
+}
+/**
+ * main - program that prints string,
+ * followed by a new line
+ * Return: 1
+ */
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
 /**
  * main - program that prints _putchar,
  * followed by a new line
  * Return: Always 0
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+	ft_putstr("_putchar\n");
 }
-
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+# include <stdio.h>
+# include <unistd.h>
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+#endif
