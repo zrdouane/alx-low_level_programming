29ddf87d461a2d823a1cc6b6dc9038da05cdfe3f
Zrdouane <ramirdouane@gmail.com>
2023-02-21 05:53:24 -0800

    edit func

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,12 @@
 */
 void print_alphabet(void)
 {
-int letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar (letter);
-}
-_putchar (10);
-}
+	int i = 97;
 
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
