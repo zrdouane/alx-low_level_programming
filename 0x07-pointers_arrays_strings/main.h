41cf1772b57d86f7f021333509729a6cde9f697e
Zrdouane <ramirdouane@gmail.com>
2023-03-06 04:52:25 -0800

    7. Chess is mental torture

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <unistd.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -8,4 +9,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 #endif /* MAIN_H */
