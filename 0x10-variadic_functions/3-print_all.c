c9f77458ad2ddf03a6375b5844bf931557fbe6d1
Zrdouane <ramirdouane@gmail.com>
2023-03-23 15:33:05 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,48 @@
 #include "variadic_functions.h"
-/**
- *_putchar - print a charcter
- *@arg: argmcount
- */
-void _putchar(va_list arg)
-{
-	char string;
-		string = va_arg(arg, int);
-	printf("%c", string);
-}
-/**
- * _int - print number
- * @arg: argcount
- */
-void _int(va_list arg)
-{
-	int number;
-		number = va_arg(arg, int);
-	printf("%d", number);
-}
-/**
- *_float - print float
- *@arg: argcount
- */
-void _float(va_list arg)
-{
-	float number;
 
-	number = va_arg(arg, double);
-	printf("%f", number);
-}
 /**
- *_string - print string
- *@arg: argcount
- */
-void _string(va_list arg)
-{
-	char *string;
-		string = va_arg(arg, char *);
-
-	if (string == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", string);
-}
-/**
- *print_all -  function that prints anything.
- * @format : format of input
- * Return: nothing
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
 	int i = 0;
-	int j = 0;
-	char *separator = "";
-	printer_t function[] = {
-		{"c", _putchar},
-		{"i", _int},
-		{"f", _float},
-		{"s", _string}};
-	va_start(args, format);
+	char *str, *sep = "";
 
-	while (format && (*(format + i)))
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		j = 0;
-		while (j < 4 && (*(format + i) != *(function[j].sym)))
-			j++;
-		if (j < 4)
+		while (format[i])
 		{
-			printf("%s", separator);
-			function[j].print(args);
-			separator = ", ";
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		i++;
 	}
+
 	printf("\n");
-	va_end(args);
+	va_end(list);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,23 +2,9 @@
 #define VARIADIC_FUNCTION_H
 #include <stdarg.h>
 #include <stdio.h>
-/**
- *struct printer - printe
- *@sym : smbole
- *@print : print
- */
-typedef struct printer
-{
-	char *sym;
-	void (*print)(va_list arg);
-} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void _putchar(va_list arg);
-void _int(va_list arg);
-void _float(va_list arg);
-void _string(va_list arg);
 void print_all(const char * const format, ...);
 #endif
