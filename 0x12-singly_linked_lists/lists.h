0078770a23bde302535009d3b1323d1770ad3701
Zrdouane <ramirdouane@gmail.com>
2023-03-30 13:54:27 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * struct list_s - single linked list
+ * @str: string
+ * @len: lenght of the string
+ * @next: next node
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
