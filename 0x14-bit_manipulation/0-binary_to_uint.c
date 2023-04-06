be536516dc3511e2e22dd40abbdd17478d5b2038
Zrdouane <ramirdouane@gmail.com>
2023-04-06 14:10:27 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary to an uint
+ * @b: binary number
+ *
+ * Return: converted value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int str_len = 0, base = 1;
+
+	if (!check_valid_string(b))
+		return (0);
+
+	while (b[str_len] != '\0')
+		str_len++;
+
+	while (str_len)
+	{
+		decimal += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
+	}
+	return (decimal);
+}
+
+/**
+ * check_valid_string - checks if a string has only 0's and 1's
+ * @b: string.
+ *
+ * Return: succes if string is valid, null otherwise
+ */
+int check_valid_string(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '1' && *b != '0')
+			return (0);
+		b++;
+	}
+	return (1);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+int check_valid_string(const char *b);
+#endif
