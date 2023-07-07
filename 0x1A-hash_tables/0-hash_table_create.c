a1d9b60ed878427dc629858e330fe69239b5e495
zrdouane <ramirdouane@gmail.com>
2023-07-07 04:33:39 -0700

    hecking for memory allocation failure

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,10 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
+	{
 		return (NULL);
+	}
 
 	hash_table->size = size;
+
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
+
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
@@ -22,7 +26,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (i = 0; i < size; i++)
+	{
 		hash_table->array[i] = NULL;
+	}
 
 	return (hash_table);
 }
