21dd5900347f678cf252929e42f83992a6c230e4
zrdouane <ramirdouane@gmail.com>
2023-07-07 04:39:38 -0700

    fix the code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,29 +7,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
+	unsigned long int i = 0;
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 	{
+		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
 
 	hash_table->size = size;
-
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
+		fprintf(stderr, "Error: malloc failed\n");
 		free(hash_table);
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
-	{
+	for (; i < size; i++)
 		hash_table->array[i] = NULL;
-	}
 
 	return (hash_table);
 }
-
