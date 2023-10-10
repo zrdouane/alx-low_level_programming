03076256f51801ef047b9d65262c8138c39b5864
zrdouane <ramirdouane@gmail.com>
2023-10-10 11:59:35 +0100

    bonus

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -7,12 +7,30 @@
  * @value : the value to search for.
  * Return: index of value or -1 if not found.
 */
-
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (!array || !size)
-		return (-1);
-	else if ((size == 1) && (*array == value))
-		return (0);
-	return (binary_search_index(array, 0, size - 1, value));
+	size_t index = 0, s = size - 1, move, r;
+
+	if (array)
+	{
+		while (index <= s)
+		{
+			printf("Searching in array: ");
+			for (r = index; r < s; r++)
+				printf("%d, ", array[r]);
+			printf("%d\n", array[r]);
+			move = (index + s) / 2;
+			if (array[move] == value)
+			{
+				if (move == 0 || array[move - 1] < value)
+					return (move);
+				s = move;
+			}
+			else if (array[move] < value)
+				index = move + 1;
+			else
+				s = move;
+		}
+	}
+	return (-1);
 }
