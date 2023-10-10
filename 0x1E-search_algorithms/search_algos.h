f9d114ea14bc6e50aeb13516011247d2bb99d97e
zrdouane <ramirdouane@gmail.com>
2023-10-10 11:54:58 +0100

    header edit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
+int binary_search_index(int *array, size_t left, size_t right, int value);
+
+
 #endif
